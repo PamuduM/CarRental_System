c0cdee7ccf28773fe856fd4d44bdb41cf6c21ce7
Pamudu Mihiranga <121780040+PamuduM@users.noreply.github.com>
2023-10-22 13:20:10 -0700

    Create UserCredentials

---
+++ Back-End/src/main/java/lk/easy/carRental/entity/User_credentials.java
@@ -0,0 +1,23 @@
+package lk.easy.carRental.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@Entity
+public class User_credentials {
+    @Id
+    private String username;
+
+    private String password;
+
+    private String role;
+}
