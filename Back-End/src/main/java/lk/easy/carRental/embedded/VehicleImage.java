d4b8c963f5323549c3780fe60a44c73b53a13335
Pamudu Mihiranga <121780040+PamuduM@users.noreply.github.com>
2023-10-22 13:06:17 -0700

    Update VehicleImage.java

--- Back-End/src/main/java/lk/easy/carRental/embedded/VehicleImage.java
+++ Back-End/src/main/java/lk/easy/carRental/embedded/VehicleImage.java
@@ -1,23 +1,3 @@
 package lk.easy.carRental.embedded;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-
-import javax.persistence.Embeddable;
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
-@ToString
-@Embeddable
-public class VehicleImage {
-    private String front;
-
-    private String back;
-
-    private String side;
-
-    private String interior;
 }
