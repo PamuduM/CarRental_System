d14200fb5220b0e2e989f3b000cb55700183e39c
Pamudu Mihiranga <121780040+PamuduM@users.noreply.github.com>
2023-10-30 22:01:06 -0700

    Update HomeController

--- Front-End/controller/HomeController.js
+++ Front-End/controller/HomeController.js
@@ -1,3 +1,79 @@
+let baseUrl = "http://localhost:8085/Back_End_war/";
+$(function () {
+    $("#resetPasswordForm").css('display', 'none');
+    $('#sortingContainer').children().css('display', 'none');
+    loadAllCarsDetails();
+});
+
+$("#btnForgotPassword").click(function () {
+    $("#loginFormHeader").text("Reset Password Form");
+    $("#loginFormBody").css('display', 'none');
+    $("#resetPasswordForm").css('display', 'block');
+});
+
+$("#btnCloseLoginForm").click(function () {
+    $("#loginFormHeader").text("Login Form");
+    $("#loginFormBody").css('display', 'flex');
+    $("#resetPasswordForm").css('display', 'none');
+});
+
+$("#btnCancelResetPassword").click(function () {
+    $("#loginFormHeader").text("Login Form");
+    $("#loginFormBody").css('display', 'flex');
+    $("#resetPasswordForm").css('display', 'none');
+
+    $("#txtResetUsername").val("");
+    $("#txtResetNewPassword").val("");
+    $("#txtResetConfirmPassword").val("");
+});
+
+// Set Nic Image
+$("#uploadNicImage").on('change', function (e) {
+    let file = e.target.files;
+    if (FileReader && file && file.length) {
+        let reader = new FileReader();
+        reader.onload = function () {
+            $("#customerNicImage").attr('src', reader.result);
+        }
+        reader.readAsDataURL(file[0]);
+    }
+});
+
+// Set License Image
+$("#uploadLicenseImage").on('change', function (e) {
+    let file = e.target.files;
+    if (FileReader && file && file.length) {
+        let reader = new FileReader();
+        reader.onload = function () {
+            $("#customerLicenseImage").attr('src', reader.result);
+        }
+        reader.readAsDataURL(file[0]);
+    }
+});
+
+$("#btnCancel_registerForm").on('click', function () {
+    clearCustomerRegistrationFields();
+});
+
+function clearCustomerRegistrationFields() {
+    $("#txtCustomerName").val("");
+    $("#txtCusUsername").val("");
+    $("#txtCusPassword").val("");
+    $("#txtCustomerAddress").val("");
+    $("#txtCustomerContact").val("");
+    $("#txtCustomerEmail").val("");
+    $("#txtCustomerNic").val("");
+    $("#txtCustomerLicenseNo").val("");
+
+    // Clear file choosers
+    $("#uploadNicImage").val("");
+    $("#uploadLicenseImage").val("");
+
+    // Clear images
+    $("#customerNicImage").attr('src', "");
+    $("#customerLicenseImage").attr('src', "");
+}
+
 // User Login
 $("#btnUserLogin").on('click', function () {
     let user = null;
